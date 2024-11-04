33a68dccb356ed098a2272a85a2a1bd5d910e1f3
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-04 19:55:48 +0300

    Solved

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const book = {
+    title: "JavaScript: The Definitive Guide",
+    author: "David Flanagan",
+    publishedYear: 2020,
+    genre: "Programming",
+};
+
+book.pageCount = 1096
+book.iSBN = "978-1491952023";
+book.publishedYear = 2021;
+
+
+book.author = ["David Flanagan", "Another Author"];
+book.reviews = [{
+    reviewer: "Book Critic",
+    comment: "A comprehensive guide to JavaScript"
+}]
+
+
+
+
+
+
+
