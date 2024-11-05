38cf6a0529692876ef67bcb41559e72f3dbcbd39
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-05 19:15:03 +0300

    Final Submission

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const book = {
     genre: "Programming",
 };
 
+console.log (`${book.title} - ${book["publishedYear"]}`);
+
+
 book.pageCount = 1096
 book.iSBN = "978-1491952023";
 book.publishedYear = 2021;
@@ -15,7 +18,9 @@ book.author = ["David Flanagan", "Another Author"];
 book.reviews = [{
     reviewer: "Book Critic",
     comment: "A comprehensive guide to JavaScript"
-}]
+}];
+
+
 
 
 
