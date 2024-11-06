4225a67fb78bfddbef1211412d826c1ae2202be9
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-06 15:30:13 +0300

    Final Submission

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ book.pageCount = 1096
 book.iSBN = "978-1491952023";
 book.publishedYear = 2021;
 
+console.log(book);
+
 
 book.author = ["David Flanagan", "Another Author"];
 book.reviews = [{
     reviewer: "Book Critic",
     comment: "A comprehensive guide to JavaScript"
 }];
+
+console.log(book);
+
 
 
 
